214adae02df2971d46f1ab8fe2e135e96cda647c
“tgrigoriou” <github.ancestry.com>
2019-02-24 17:24:54 -0800

    try again

--- Project/app.js
+++ Project/app.js
@@ -10,8 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,14 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
-// server.get('*', function(req, res){
-//   res.sendFile('index.html');
-// });
+app.get('*', function(req, res){
+  res.sendFile('index.html');
+});
 
-/* final catch-all route to index.html defined last */
-app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-})
+// /* final catch-all route to index.html defined last */
+// app.get('/*', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// })
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
