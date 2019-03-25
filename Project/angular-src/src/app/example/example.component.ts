744f8ff1a1b8366c956d345f84613ea44443623f
tasogrigoriou <taso.grigoriou@gmail.com>
2019-03-25 12:03:34 -0700

    didn't add example component for some reason!

---
+++ Project/angular-src/src/app/example/example.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {Example, ExampleService} from '../core/services/example.service';
+import {DataSource} from "@angular/cdk/collections";
+
+@Component({
+  selector: 'app-example',
+  templateUrl: './example.component.html',
+  styleUrls: ['./example.component.css']
+})
+export class ExampleComponent implements OnInit {
+
+  examples: Example[] = [];
+  dataSource = new ExampleDataSource(this.exampleService);
+
+  displayedColumns: string[] = ['ID', 'Name'];
+
+  constructor(private exampleService: ExampleService) {}
+
+  ngOnInit() {
+    this.getExamples();
+  }
+
+  getExamples() {
+    this.exampleService.getExamples()
+      .subscribe(response => {
+        console.log(response);
+        this.examples = response;
+      }, err => {
+        console.log(err);
+      });
+  }
+}
+
+export class ExampleDataSource extends DataSource<any> {
+  constructor(private exampleService: ExampleService) {
+    super();
+  }
+
+  connect() {
+    return this.exampleService.getExamples();
+  }
+
+  disconnect() {
+
+  }
+}
