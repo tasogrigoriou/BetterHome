d80c4b4cb6a0f5c88ace91e7ad63ca8ddc8ed97d
Austin Sy-Velasco <iamaustinsy@gmail.com>
2019-04-02 16:21:20 -0700

    Login now uses password encryption

--- Project/controllers/login.js
+++ Project/controllers/login.js
@@ -2,36 +2,29 @@ var express = require('express');
 var router = express.Router();
 var database = require('../models/cloudsql');
 
-router.get('/', async function(req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
-    //Tried the login in a different format for variety :D
-    const result = database.query('SELECT * FROM users WHERE email = ?', [email], function(err, result){
-            if(err){
-                res.json({
-                    status: false,
-                    message: "Error"
-                })
-            } else{
-                if(result.length > 0){
-                    if(password = result[0].password){
-                        res.json({
-                            status: true,
-                            message: "Logged in"
-                        })
-                    }else{
-                        res.json({
-                            status: false,
-                            message: "Password is incorrect"
-                        });
-                    }
-                }else{
-                    res.json({
-                        status: false,
-                        message: "Email does not exist"
-                    });
-                }
-            }
+//Password encryption tool; npm install bcrypt
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
+router.post('/', async function(req, res) {
+    database.Users.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(function(user) {
+        if(!user){
+            res.redirect('/');
+        }else{
+            bcrypt.compare(req.body.password, user.password, function(err, result){
+                if(result == true){
+                    console.log("Logged in!")
+                    res.redirect('/home');
+                }else{
+                    res.send('Incorrect Password');
+                    res.redirect('/');
+                }
+            });
+        }
     });
+
 });
