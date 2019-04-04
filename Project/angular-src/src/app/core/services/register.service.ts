b70358b18d5e00ebe330e34c7fd7541099ece055
Cole <mtormey@mail.sfsu.edu>
2019-04-03 17:25:22 -0700

    added register.service.ts, but unsure of correctness. Adding it here for now

---
+++ Project/angular-src/src/app/core/services/register.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = '/api/register';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private http: HttpClient) { }
+
+  /** CRUD functions (create, retrieve, update, delete) **/
+
+  get(): Observable<Register[]> {
+    console.log(apiUrl);
+    return this.http.get<Register[]>(apiUrl, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  post(data): Observable<any> {
+    return this.http.post(apiUrl, data, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  update(id: string, data): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.put(url, data, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  delete(id: string): Observable<{}> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.delete(url, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
+}
+
+export interface Register {
+  id: string;
+  name: string;
+}
