7ca42a7b38b8ee61d4fd2aff56e18068e1917c83
“tgrigoriou” <github.ancestry.com>
2019-04-10 21:31:07 -0700

    Adding search for home page, query results from backend database

--- Project/angular-src/src/app/home/home.component.ts
+++ Project/angular-src/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from "@angular/material";
+import {RegisterDialog} from "../register/register.dialog";
+import {SearchListingsService} from "../core/services/search.listings.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -8,28 +12,77 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   listingSearch: ListingSearch;
+  listings: Listing[];
 
-  constructor() {}
+  isLoaded = true;
+
+  constructor(
+    private router: Router,
+    private searchService: SearchListingsService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.listingSearch = {
-      city: '',
-      listingType: '',
-      numBedrooms: 0,
-      numBathrooms: 0
+      city: ''
     }
   }
 
   onSearchClick() {
     if (!this.listingSearch.city.length) {
-      console.log("city is empty");
+      this.openDialog('Please enter some text for the city field');
+    }
+    else {
+      this.isLoaded = false;
+      this.searchService.getSearchListings(this.listingSearch)
+        .subscribe(listings => {
+        this.isLoaded = true;
+        this.saveListings(listings);
+        this.openDialog('Successfully retrieved Listings!', true);
+      },
+            err => {
+          this.isLoaded = true;
+          this.openDialog('Unable to retrieve any listings based on your search. Please try again');
+      });
+    }
+  }
+
+  saveListings(listings: Listing[]) {
+    this.searchService.listings = listings;
+  }
+
+  openDialog(message: string, subscribe: boolean = false) {
+    const dialog = this.dialog.open(RegisterDialog, {
+      width: '250px',
+      data: {
+        message: message
+      }
+    });
+    if (subscribe) {
+      dialog.afterClosed().subscribe(result => {
+        this.router.navigate(['/search-results']);
+      });
     }
   }
 }
 
 export interface ListingSearch {
   city: string;
+  listingType?: string;
+  numBedrooms?: number;
+  numBathrooms?: number;
+}
+
+export interface Listing {
+  Lid: number;
+  DisplayBoard_boardId: number;
+  title: string;
   listingType: string;
+  price: number;
+  city: string;
+  zipCode: number;
+  street: string;
+  forSale: boolean;
   numBedrooms: number;
-  numBathrooms: number;
+  numBathrooms: string;
 }
--- Project/app.js
+++ Project/app.js
@@ -12,6 +12,7 @@ var database = require('./models/cloudsql');
 
 var apiExampleRouter = require('./controllers/example');
 var apiLoginRouter = require('./controllers/login');
+const apiSearchRouter = require('./controllers/search');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,7 +25,9 @@ app.use('/about-us', express.static(path.join(__dirname, 'public')));
 app.use('/example', express.static(path.join(__dirname, 'public')));
 app.use('/register', express.static(path.join(__dirname, 'public')));
 app.use('/login', express.static(path.join(__dirname, 'public')));
+app.use('/search-results', express.static(path.join(__dirname, 'public')));
 
+app.use('/api/search', apiSearchRouter);
 app.use('/api/example', apiExampleRouter);
 app.use('/api/register',registerController);
 app.use('/api/login', apiLoginRouter);
