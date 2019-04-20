4a120e232b66045979d5a1dba73bcc6028df9894
liwanggao <lgao2@mail.sfsu.edu>
2019-04-20 00:05:15 -0700

    add search field and options to the listing page

--- Project/angular-src/src/app/property/property.module.ts
+++ Project/angular-src/src/app/property/property.module.ts
@@ -5,12 +5,14 @@ import {CommonModule} from "@angular/common";
 import {PropertyRoutingModule} from "./property-routing.module";
 import {MatMenuModule} from '@angular/material/menu';
 import {PropertySingleListingComponent} from "./property.singleListing.component";
-import {MatCheckboxModule, MatListModule, MatSidenavModule, MatToolbarModule,MatCardModule} from "@angular/material";
-
-
+import {MatCheckboxModule, MatListModule, MatSidenavModule, MatToolbarModule,MatCardModule,MatOptionModule,MatSelectModule} from "@angular/material";
+import {MatFormFieldModule,} from '@angular/material/form-field';
 
 @NgModule({
   imports: [
+    MatSelectModule,
+    MatOptionModule,
+    MatFormFieldModule,
     MatCardModule,
     MatMenuModule,
     MatCheckboxModule,
