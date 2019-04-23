84aa4d6eaa261a3aed6839ef742cda59eb57efe3
Cole <mtormey@mail.sfsu.edu>
2019-04-22 22:22:37 -0700

    added 'add to favorites' button with (click) event

--- Project/angular-src/src/app/property/property.singleListing.component.ts
+++ Project/angular-src/src/app/property/property.singleListing.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {PropertyComponent} from "./property.component";
 import {RegisterService} from "../core/services/register.service";
 import {Member} from "../core/services/members.service";
 import {Listing} from "../home/home.component";
 import {ListingsService} from "../core/services/listings.service";
+import {LoginUser} from "../core/services/login.service";
+import {RegisterComponent} from "../register/register.component";
+import {DragScrollComponent} from "ngx-drag-scroll/lib";
+
 
 @Component({
   selector: 'app-property',
@@ -13,13 +17,19 @@ import {ListingsService} from "../core/services/listings.service";
 })
 export class PropertySingleListingComponent implements OnInit {
   listing: Listing;
+  user: LoginUser;
+
+  @ViewChild('drag_scroll', { read: DragScrollComponent }) dragScroll: DragScrollComponent;
 
   constructor(
     private route: ActivatedRoute,
-    private listingsService: ListingsService) { }
+    private listingsService: ListingsService
+  ) { }
 
 
   ngOnInit() {
+    localStorage.getItem('userLogin')
+
     console.log(this.route.snapshot.params);
     this.route.paramMap.subscribe(params => {
       console.log(params);
@@ -28,6 +38,7 @@ export class PropertySingleListingComponent implements OnInit {
       this.listing = this.listingsService.getListingById(listingId);
       console.log(this.listing);
     });
+
   }
 
   forSale(){
@@ -35,4 +46,36 @@ export class PropertySingleListingComponent implements OnInit {
       return "sale";
     return "rent";
   }
+
+  showDivs(n, photos) {
+
+
+
+    // this.document.getElementById("listingPhotos").src = this.listing.propertyPhotos[1];
+
+    // this.showDivs(this.listing.slideIndex += n);
+    // if (this.slideIndex + n > this.listing.propertyPhotos.length )
+    //   this.slideIndex = 0;
+    // if (this.slideIndex + n < 0) {this.slideIndex = this.listing.propertyPhotos.length}
+    // if(n == 0)
+    //   return this.listing.propertyPhotos[0];
+    // else
+    //   return this.listing.propertyPhotos[this.slideIndex + n];
+    // var x = document.getElementById("listingPhotos")[0];
+    // if(this.listing.propertyPhotos.length + n > this.listing.propertyPhotos.length)
+    //   x.src = this.listing.propertyPhotos[1];
+  }
+
+  moveLeft() {
+    this.dragScroll.moveLeft();
+  }
+
+  moveRight() {
+    this.dragScroll.moveRight();
+  }
+
+  addToFavorites() {
+
+  }
+
 }
