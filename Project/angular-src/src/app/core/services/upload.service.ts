409f2c69e50dab49b3d5f25b0000eb6871a92ec9
tasogrigoriou <taso.grigoriou@gmail.com>
2019-04-25 00:04:27 -0700

    removed unneeded code

--- Project/angular-src/src/app/core/services/upload.service.ts
+++ Project/angular-src/src/app/core/services/upload.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 
-const singleApiUrl = '/api/upload';
-const multipleApiUrl = '/api/upload/multiple';
-
-
+const apiUrl = '/api/upload';
 @Injectable({
   providedIn: 'root'
 })
@@ -27,33 +24,7 @@ export class UploadService {
         }
       };
 
-      xhr.open('POST', singleApiUrl);
-      xhr.setRequestHeader('listingId', String(listingId));
-      xhr.send(fd);
-    });
-  }
-
-  uploadImages(files: File[], listingId: number = 0) {
-    let fd = new FormData();
-    let xhr = new XMLHttpRequest();
-    for (let i = 0; i < files.length; i++) {
-      fd.append('file', files[i], files[i].name);
-    }
-
-    return new Promise((resolve, reject) => {
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            console.log('images uploaded');
-            resolve(xhr.response);
-          } else {
-            console.log('failed to upload images');
-            reject(xhr.response);
-          }
-        }
-      };
-
-      xhr.open('POST', multipleApiUrl);
+      xhr.open('POST', apiUrl);
       xhr.setRequestHeader('listingId', String(listingId));
       xhr.send(fd);
     });
